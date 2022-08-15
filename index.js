11b85bd73408c04c986ae587bc72cda87cbcbdc4
Noela-Murugi <murugin254@gmail.com>
2022-08-15 22:23:18 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+
+//multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = parseInt(num1 * num2);
+multiply;
+
+
+//random
+let random = Math.ceil((Math.random() * 10) + 1);
+
+//mod
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+//max
+let max = Math.max(11, 2, 3, 6, 15, 20);
+max;
